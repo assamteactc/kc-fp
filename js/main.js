f26da51ffa5a449bb9bc439f9cccb8f432d90097
assamteactc <66681503+assamteactc@users.noreply.github.com>
2020-06-12 20:08:19 +0900

    push

--- js/main.js
+++ js/main.js
@@ -1,19 +1,14 @@
-function readMyship {
-  var target = '#myship_list';
-            var csv = $.csv.toArrays(data);
-            var insert = '';
-            $(csv).each(function() {
-                if (this.length > 0) {
-                    insert += '<tr>';
-                    $(this).each(function() {
-                        insert += '<td>' + this + '</td>';
-                    });
-                    insert += '</tr>';
-                }
-            });
-            $(target).append(insert);
-        }
-        var csvfile = '../data/MY_SHIP.csv';
-        $(function(){
-            $.get(csvfile, readCsv, 'text');
-        });
+$(window).load(function () {
+    $.get('../data/MY_SHIP.csv',function(data){
+        var csv = $.csv()(data);
+        var itemlist = '';
+        $(csv).each(function(index){
+                itemlist += '<li class="list-item">';
+                itemlist += '<p class="date">' + this[0] + '</p>';
+                itemlist += '<p class="title"><a href="' + this[1] + '">' + this[2] + '</a></p>';
+                itemlist += '<p class="body">' + this[3] + '</p>';
+                itemlist += '</li>';
+	})
+	$("#myship_list").append(itemlist);
+    });
+});
